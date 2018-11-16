7fb24f20680d38d5174aececd63fda0b2ae0cde7
David Kebler <d@kebler.net>
2018-11-16 09:46:41 -0800

    added ipset and redirect to kernel config.  Put .config's under version control.  Improved install script.  Added distclean script added toolchain path to cfgedit script.

--- distclean.sh
+++ distclean.sh
@@ -2,4 +2,7 @@
 export ARCH=arm64
 export CROSS_COMPILE=aarch64-linux-gnu-
 export PATH=$PATH:/mnt/AllData/hacking/toolchain/gcc-linaro-5.2-2015.11-2-x86_64_aarch64-linux-gnu/bin
-make menuconfig
+# run this if you get silentoldconfig error
+# make sure that there are no needed files without correct permission
+# this can cause the slientoldconfig error.
+make distclean
--- install.sh
+++ install.sh
@@ -8,28 +8,21 @@ echo "module directory $MODDIR"
 echo "install location /mnt/$DIR"
 cd /mnt/$DIR/boot
 DATE=$(date +"%d%m%Y")
+# backup current kernel, device tree, config and modules
 N=1
 # Increment $N as long as a directory with that name exists
 while [[ -d "./$DATE-$N" ]] ; do
     N=$(($N+1))
 done
 sudo mkdir "./$DATE-$N"
-sudo cp Image "./$DATE-$N"
-sudo cp armada-3720-espressobin.dtb "./$DATE-$N"
+sudo mv Image "./$DATE-$N"
+sudo mv armada-3720-espressobin.dtb "./$DATE-$N"
+sudo mv .config "./$DATE-$N"
+sudo mv /mnt/$DIR/lib/modules/$MODDIR* "./$DATE-$N"
+# copy new kernel and device tree to /boot
 cd $KD/arch/arm64/boot
 sudo cp Image /mnt/$DIR/boot
 sudo cp ./dts/marvell/armada-3720-espressobin.dtb /mnt/$DIR/boot
-cd /mnt/$DIR/lib/modules
-if [ -d "$MODDIR" ]; then
-  echo
-  DATE=$(date +"%d%m%Y")
-  N=1
-  # Increment $N as long as a directory with that name exists
-  while [[ -d "./$DATE-$N" ]] ; do
-      N=$(($N+1))
-  done
-  sudo mkdir "./$DATE-$N"
-  sudo mv "./$MODDIR"  "./$DATE-$N"
-fi
+sudo cp $KD/.config /mnt/$DIR/boot
 cd $KD
 sudo make ARCH=arm64 modules_install INSTALL_MOD_PATH=/mnt/$DIR/
